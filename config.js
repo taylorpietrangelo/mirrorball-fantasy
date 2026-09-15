af036c3849d98750aabd1858349507bacf30157e
taylorpietrangelo <taylor.pietrangelo@gmail.com>
2026-09-15 11:02:13 -0400

    Publish Mirrorball Fantasy v14

---
+++ config.js
@@ -0,0 +1,1 @@
+window.MIRRORBALL_CONFIG={supabaseUrl:"https://umqjplzvfwdggjcroawa.supabase.co",supabasePublishableKey:"sb_publishable_pJS8rlARhH4cLPdKIsaxug_rRf28LGw",leagueId:"dwts-2026",leagueCode:"MIRROR26"};
